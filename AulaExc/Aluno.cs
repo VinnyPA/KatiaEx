802d605151a0cbdaf216a22160c29a94405cca21
curso.ads <curso.ads@LAB06-35>
2022-08-29 20:05:03 -0300

    Adicionar arquivos de projeto.

---
+++ AulaExc/Aluno.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaExc
+{
+    internal class Aluno
+    {
+        //declaração de atributo
+        string nome;
+        string cpf;
+        string endereco;
+        int ra;
+        // declaração de metodos getter e setter
+        public void SetNome(string nome)
+        {
+            this.nome = nome;
+        }
+        public string GetNome()
+        {
+            return this.nome;
+        }
+
+        public void SetCof(string cpf)
+        {
+            this.cpf = cpf;
+        }
+        public string GetCpf()
+        {
+            return this.cpf;
+        }
+
+        public void SetEndereco(string endereco)
+        {
+            this.endereco = endereco;
+        }
+        public string GetEndereco()
+        {
+            return this.endereco;
+        }
+
+        public void SetRa(int ra)
+        {
+            this.ra = ra;
+        }
+        public int GetRa()
+        {
+            return this.ra;
+        }
+
+
+    }
+}
